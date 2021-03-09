faf34b128e9886ff1b1695781047006f3a61d0a3
Meiryland Melo <meirylandmelo@gmail.com>
2021-03-09 16:10:53 -0300

    adicionando exercicio

---
+++ exercicio-componetes-classes/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Notebook from "./components/Notebook";
+
+function App() {
+  return (
+    <div className="App">
+      <Notebook />
+    </div>
+  );
+}
+
+export default App;
